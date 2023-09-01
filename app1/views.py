cddb37f557572ad7f251623201e8f0c61f7c581a
catalina-romero <catalina.romero12@inacapmail.cl>
2023-09-01 12:15:15 -0400

    Configuracion de pag de inicio

--- app1/views.py
+++ app1/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def displayDateTime(request):
+    import datetime
+    now = datetime.datetime.now()
+    return HttpResponse("<h1>Hello World</h1><br><h2>Current Date and Time is: "+str(now)+"</h2>")
